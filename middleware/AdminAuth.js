1835726a124629a914452771c80abe9635ea52b6
brunobandelli <brunobandelli@gmail.com>
2021-08-31 22:13:29 -0300

    Sistema de autorização de adm.

---
+++ middleware/AdminAuth.js
@@ -0,0 +1,34 @@
+var jwt = require("jsonwebtoken");
+
+var secret = "sauidapbdas"
+
+module.exports = function(req,res,next){
+    const authToken = req.headers['authorization']
+
+    if(authToken != undefined){
+
+        const bearer = authToken.split(' ');
+        var token = bearer[1];
+
+        try{
+            var decoded = jwt.verify(token,secret);
+            
+            if(decoded.role == 1){
+                next();
+            }else{
+                res.status(403);
+                res.send("Você não tem permissão para isso!")
+                return
+            }
+
+        }catch(err){
+            res.status(403);
+            res.send("Você não está autenticado")
+            return
+        }
+    }else{
+        res.status(403);
+        res.send("Você não está autenticado");
+        return;
+    }
+}
--- routes/routes.js
+++ routes/routes.js
@@ -4,13 +4,14 @@ var router = express.Router();
 var HomeController = require("../controllers/HomeController");
 var UserController = require("../controllers/UserController");
 const User = require("../models/User");
+var AdminAuth = require("../middleware/AdminAuth")
 
 router.get('/', HomeController.index);
 router.post('/user',UserController.create);
-router.get('/user',UserController.index);
-router.get('/user/:id',UserController.findUser);
-router.put('/user',UserController.edit);
-router.delete('/user/:id',UserController.remove);
+router.get('/user',AdminAuth,UserController.index);
+router.get('/user/:id',AdminAuth,UserController.findUser);
+router.put('/user',AdminAuth,UserController.edit);
+router.delete('/user/:id',AdminAuth,UserController.remove);
 router.post("/recoverpassword",UserController.recoverPassword);
 router.post("/changepassword",UserController.changePassword);
 router.post("/login",UserController.login)
